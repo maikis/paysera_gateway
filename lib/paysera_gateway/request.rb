8e81f752e649c6d82d93e75e6d80a27aab9f4f69
Mykolas Grinevicius <mykolas@grineviciai.eu>
2018-03-30 21:19:02 +0300

    Add request class

---
+++ lib/paysera_gateway/request.rb
@@ -0,0 +1,38 @@
+require 'net/http'
+
+module PayseraGateway
+  # Request constructor class.
+  class Request
+    attr_reader :params, :sign_password
+
+    PAYSERA_URI = 'https://www.paysera.com/pay/'.freeze
+
+    def initialize(params)
+      @params = params_validator.validate(params)
+      @sign_password = params.delete(:sign_password)
+    end
+
+    def build
+      { data: base64_encoded_data, sign: sign }
+    end
+
+    private
+
+    def params_validator
+      PayseraGateway::ParamsValidator
+    end
+
+    def sign
+      Digest::MD5.hexdigest(base64_encoded_data + sign_password)
+    end
+
+    def base64_encoded_data
+      data = Base64.strict_encode64(encoded_params)
+      data.tr('/', '_').tr('+', '-')
+    end
+
+    def encoded_params(params_to_encode = params)
+      URI.encode_www_form(params_to_encode)
+    end
+  end
+end
---
+++ spec/lib/request_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+require 'digest/md5'
+require 'base64'
+
+RSpec.describe PayseraGateway::Request do
+  let(:params_config) do
+    config_file = File.expand_path(
+      '../../../lib/paysera_gateway/config/params.yml',
+      __FILE__
+    )
+    YAML.load_file(config_file)
+  end
+
+  let(:params) do
+    req = params_config[:required]
+    params = {}
+    req.each { |param| params[param] = param.to_s }
+    params
+  end
+
+  it 'has params' do
+    expect(described_class.new(params).params).to eq(params)
+  end
+
+  it 'extracts sign_password from params on initialization' do
+    request = described_class.new(params.dup)
+    expect(request.sign_password).to eq(params[:sign_password])
+  end
+
+  context 'validation' do
+    it 'raises ValidationError if params are missing required fields' do
+      expect { described_class.new({}) }.to raise_error do |error|
+        expect(error).to be_a(PayseraGateway::ValidationError)
+        expect(error.message).to eq('Required parameters are missing.')
+      end
+    end
+  end
+
+  describe '#build' do
+    it 'generates valid paysera request data' do
+      request = described_class.new(params.dup)
+      sign_password = params.delete(:sign_password)
+      encoded_params = URI.encode_www_form(params)
+      data = Base64.strict_encode64(encoded_params)
+      data = data.tr('/', '_').tr('+', '-')
+      sign = Digest::MD5.hexdigest(data + sign_password)
+      expect(request.build).to eq(data: data, sign: sign)
+    end
+  end
+end
