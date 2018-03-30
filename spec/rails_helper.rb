dbe04b0fa9e7a804d4df4d80494e59a3eccd3bd6
Mykolas Grinevicius <mykolas@grineviciai.eu>
2018-03-30 21:17:14 +0300

    Configure dummy for Rspec and require libs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,14 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../dummy/config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'paysera_gateway/request'
+require 'paysera_gateway/params_validator'
+require 'paysera_gateway/configuration'
+require 'paysera_gateway/errors/validation_error'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
